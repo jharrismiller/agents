5e0c42252ec789dd6ff4fca18508cebb16ad50b3
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2025-08-08 15:25:32 -0400

    Initial

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -76,8 +76,8 @@ tools = [{"type": "function", "function": record_user_details_json},
 class Me:
 
     def __init__(self):
-        self.openai = OpenAI()
-        self.name = "Ed Donner"
+        self.openai = OpenAI(base_url=os.getenv('OPENROUTER_BASE_URL'), api_key=os.getenv('OPENROUTER_API_KEY'))
+        self.name = "John Miller"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
@@ -116,7 +116,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            response = self.openai.chat.completions.create(model="openai/gpt-4.1-nano", messages=messages, tools=tools)
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
